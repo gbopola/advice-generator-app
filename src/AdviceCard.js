e49b3ceceed1706573a5c2ca03d3047a9b419239
gbopola <opeafolabi5@gmail.com>
2022-09-06 20:24:16 +0100

    first commit

---
+++ src/AdviceCard.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import dividerDesktop from './images/pattern-divider-desktop.svg'
+import dividerMobile from './images/pattern-divider-mobile.svg'
+import dice from './images/icon-dice.svg'
+export const AdviceCard = () => {
+    const [advice, setAdvice] = useState('')
+    
+        const getAdvice = async() => {
+            const response = await axios.get('https://api.adviceslip.com/advice')
+            const advice = await response.data.slip;
+            setAdvice(advice)
+        }
+
+        useEffect(() => {
+            getAdvice();
+           }, []);
+  return (
+    <div className='card'>
+        <p>ADVICE #{advice.id}</p>
+        <h2>“{advice.advice}”</h2>
+        <img src={dividerDesktop} className="divider-desktop"  />
+        <img src={dividerMobile} className="divider-mobile" />
+        <div className="dice" onClick={getAdvice}>
+        <img src={dice}   />
+        </div>
+    </div>
+  )
+}
